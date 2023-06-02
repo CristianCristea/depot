03d03a06d060c0bf0c616dcb8c2a9460c360940f
Cristian Cristea <cristiancristea.1987@gmail.com>
2023-06-02 12:24:50 +0200

    Add cart to sidebar, update in realtime

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -26,7 +26,24 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to cart_url(@line_item.cart) }
+        # The way to read this code is as follows: whenever we get a request that accepts a turbo stream response, we render a turbo stream response consisting of turbo stream replace specifying:
+
+        # an HTML element ID of cart as the element to be replaced,
+        # rendering the partial form app/views/application/_cart.html.erb
+        # using the value of @cart as the value of cart.
+
+        # format.turbo_stream do
+        #   render turbo_stream: turbo_stream.replace(
+        #     :cart, # id of HTML elem to be replaced
+        #     partial: 'layouts/cart', # partial to render
+        #     locals: { cart: @cart } # values
+        #   )
+        # end
+
+        # renders line_items#create.turbo_stream.erb
+        # @current_item is the last added item, needed for css animation
+        format.turbo_stream { @current_item = @line_item }
+        format.html { redirect_to store_index_url }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
